c90b2dd2699804290825243523cb7fdb841ac2b5
Dalvir Singh <dalvir.singh@cerner.com>
2021-04-14 00:01:00 -0400

    created models & views & connected the pieces

--- service/server/server/settings.py
+++ service/server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lq3bu#^j+(z^!x&s=&($id99c+n_0)*ju_wx&5&$1@119frkx0'
+SECRET_KEY = 'django-insecure-j-6sx1f5#4ghoc)^9inakn*+-pn3utn2g-2@nx%ozn^#=i)ba='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
